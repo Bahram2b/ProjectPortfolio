0125a4b50507cdbd3e7e93d3f5be5669114aa638
bahram <bahroom.2b@gmail.com>
2021-09-18 13:08:47 +0430

    Admin Panel working , still not the header

--- resources/views/Admin/AdminMaster.blade.php
+++ resources/views/Admin/AdminMaster.blade.php
@@ -11,6 +11,7 @@
     <!-- GLOBAL MAINLY STYLES-->
 
     <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet" />
+    <link href="{{asset('css/fontawesome.css')}}" rel="stylesheet" />
 
     <link href="{{asset('backend/vendors/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" />
     <link href="{{asset('backend/vendors/themify-icons/css/themify-icons.css')}}" rel="stylesheet" />
@@ -56,14 +57,14 @@
     </div>
     <!-- END PAGA BACKDROPS-->
     <!-- CORE PLUGINS-->
-    <script src="../../js/j" type="text/javascript"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" type="text/javascript"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js" type="text/javascript"></script>
 
-{{--    <script src="{{asset('backend/vendors/jquery/dist/jquery.min.js')}}" type="text/javascript"></script>--}}
-{{--    <script src="{{asset('backend/vendors/jquery/dist/umd/popper.min.js')}}" type="text/javascript"></script>--}}
-{{--    <script src="{{asset('backend/vendors/bootstrap/dist/js/bootstrap.min.js')}}" type="text/javascript"></script>--}}
+
+
+    <script src="{{asset('backend/vendors/jquery/dist/jquery.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('backend/vendors/jquery/dist/umd/popper.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('backend/vendors/bootstrap/dist/js/bootstrap.min.js')}}" type="text/javascript"></script>
     <script src="{{asset('backend/vendors/metisMenu/dist/metisMenu.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('backend/vendors/jquery-slimscroll/jquery.slimscroll.min.js')}}" type="text/javascript"></script>
 {{--    <script src="{{asset('backend/vendors/jquery-slimscroll/jquery.slimscroll.min.js')}} " type="text/javascript"></script>--}}
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
 {{--    <!-- PAGE LEVEL PLUGINS-->--}}
@@ -71,10 +72,14 @@
 {{--    <script src="{{asset('backend/vendors/jvectormap/jquery-jvectormap-2.0.3.min.js')}}" type="text/javascript"></script>--}}
 {{--    <script src="{{asset('backend/vendors/jvectormap/jquery-jvectormap-world-mill-en.js')}}" type="text/javascript"></script>--}}
 {{--    <script src="{{asset('backend/vendors/jvectormap/jquery-jvectormap-us-aea-en.js')}}" type="text/javascript"></script>--}}
-{{--    <!-- CORE SCRIPTS-->--}}
+    <!-- CORE SCRIPTS-->
     <script src="{{asset('backend/js/app.min.js')}}" type="text/javascript"></script>
+{{--    <script src="{{asset('js/bootstrap.min.js')}}" type="text/javascript"></script>--}}
+{{--    <script src="{{asset('js/popper.min.js')}}" type="text/javascript"></script>--}}
+{{--    <script src="{{asset('js/jquery-3.6.0.min.js')}}" type="text/javascript"></script>--}}
 
-    <script src="{{ asset('https://unpkg.com/sweetalert/dist/sweetalert.min.js')}}"></script>
+
+
 
 {{--    <!-- PAGE LEVEL SCRIPTS-->--}}
 {{--    <script src="{{asset('backend/js/scripts/dashboard_1_demo.js')}}" type="text/javascript"></script>--}}
